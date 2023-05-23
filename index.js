efb81f08e058c1d1c11f18fad3796ed0eec7cfce
Ville Nättinen <nattinen@synopsys.com>
2023-05-23 15:42:00 +0300

    Complete exercises up to part 3.8

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+morgan.token('person', function getPerson(req) {
+    return JSON.stringify(req.body)
+})
+
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
+
+let persons = [
+
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${new Date().toString()}</p>`
+    )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    if (!request.body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!request.body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    } else if (persons.find(person => person.name === request.body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+    const person = {
+        id: randomInt(),
+        name: request.body.name,
+        number: request.body.number
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const randomInt = () => {
+    return Math.floor(Math.random() * Number.MAX_SAFE_INTEGER)
+}
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
